9d6ec9515f710bc7d8a8a363190bc9e9fa6985f5
fernandosouza <nandope5@gmail.com>
2014-08-13 09:48:19 -0300

    Code organize.

--- application.js
+++ application.js
@@ -1,28 +1,30 @@
 angular.module('app', [])
 
     .controller('multipleOptionsController', ['$scope', function($scope){
-    	$scope.field = {
-            lable: 'Demo Multiple Options Field',
-            type: '1',
-            name: '' ,
-            options: [
-                {
-                    'text': 'option 1',
-                    'value': '1',
-                    'checked': false
-                },
-                {
-                    'text': 'option 2',
-                    'value': '2',
-                    'checked': false
-                },
-                {
-                    'text': 'option 3',
-                    'value': '3',
-                    'checked': true
-                }
-            ]
-        };
+    	$scope.fields = [
+            {
+                label: 'Multiple Options Field',
+                type: '2',
+                name: 'multiOptions' ,
+                selected: '2',
+                options: [
+                    {
+                        'text': 'option 1',
+                        'value': '1'
+                    },
+                    {
+                        'text': 'option 2',
+                        'value': '2'
+                    },
+                    {
+                        'text': 'option 3',
+                        'value': '3'
+                    }
+                ]
+            }
+        ];
+
+        window.fieldValues = $scope.fields;
     }])
 
     .directive('multipleOptionsField', function($compile){
@@ -31,7 +33,7 @@ angular.module('app', [])
     		templateUrl: 'template.html',
             replace: false,
             scope: {
-                field:'='
+                fields:'='
             }
     	}
     });
